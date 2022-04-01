008c61ae59d1ed4255a600790705269e287da03b
Andreas Koepf <andreas.koepf@xamla.com>
2022-04-01 18:35:08 +0200

    work on first world model test

--- tests/test_world_model.py
+++ tests/test_world_model.py
@@ -1,10 +1,12 @@
 import unittest
 
+import numpy as np
+import haiku as hk
+
 from dreamer.configuartion import DreamerConfiguration
 from dreamer.world_model import WorldModel
 
-
-from .test_rssm import Fixture
+from tests.test_rssm import Fixture
 
 
 def update_config(c: DreamerConfiguration):
@@ -18,6 +20,46 @@ def update_config(c: DreamerConfiguration):
     ))
 
 
+def model(config):
+    model = WorldModel(np.ones((64, 64, 3)), config)
+
+    def filter_state(prev_state, prev_action, observation):
+        return model(prev_state, prev_action, observation)
+
+    def generate_sequence(initial_state, policy, policy_params):
+        return model.generate_sequence(initial_state, policy, policy_params)
+
+    def observe_sequence(observations, actions):
+        return model.observe_sequence(observations, actions)
+
+    def decode(feature):
+        return model.decode(feature)
+
+    def init(observations, actions):
+        return model.observe_sequence(observations, actions)
+
+    return init, (filter_state, generate_sequence, observe_sequence, decode)
+
+
+class Fixture2(Fixture):
+    def __init__(self):
+        super().__init__()
+        update_config(self.config)
+        self.model = hk.multi_transform(lambda:model(self.config))
+        self.params = self.model.init(
+            self.seed, self.dummy_observations, self.dummy_actions)
+
+
 class TestWorldModel(unittest.TestCase):
     def test_call(self):
-        f = Fixture()
+        f = Fixture2()
+
+        call, *_ = f.model.apply
+        subkey = f.rng_split()
+        (prior, posterior), state = call(
+            f.params,
+            subkey,
+            tuple(map(lambda x: x[0], f.dummy_state)),
+            f.dummy_actions[0, 0],
+            f.dummy_observations[0, 0]
+        )
