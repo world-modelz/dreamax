a3216067ce762a59961195fbe99a23a52f921a9d
XMaster96 <XMaster96@gmx.de>
2022-04-02 14:13:42 +0200

    rssm

--- dreamer/rssm.py
+++ dreamer/rssm.py
@@ -8,137 +8,149 @@ import jax
 import jax.nn as jnn
 import jax.numpy as jnp
 from tensorflow_probability.substrates import jax as tfp
-tfd = tfp.distributions
 
+tfd = tfp.distributions
 
 State = Tuple[jnp.ndarray, jnp.ndarray]
 Action = jnp.ndarray
 Observation = jnp.ndarray
 
 
-class Prior(hk.Module):
+class Z_Head_Layer(hk.Module):
     def __init__(self, config: RssmConfig, initialization: str):
         super().__init__()
-        self.c = config
+        self.config = config
         self.initialization = initialization
 
     def __call__(self, prev_state: State, prev_action: Action) -> Tuple[tfd.MultivariateNormalDiag, State]:
+
+        # Unstack state
         stoch, det = prev_state
         cat = jnp.concatenate([prev_action, stoch], -1)
-        x = jnn.elu(hk.Linear(self.c.deterministic_size, name='h1',
-                    w_init=initializer(self.initialization))(cat))
-        x, det = hk.GRU(
-            self.c.deterministic_size,
-            w_i_init=initializer(self.initialization),
-            w_h_init=hk.initializers.Orthogonal()
-        )(x, det)
-        x = jnn.elu(hk.Linear(self.c.hidden, name='h2',
-                    w_init=initializer(self.initialization))(x))
-        x = hk.Linear(self.c.stochastic_size * 2, name='h3',
-                      w_init=initializer(self.initialization))(x)
+
+        x = hk.Linear(self.config.deterministic_size, name='h1', w_init=initializer(self.initialization))(cat)
+        x = jnn.elu(x)
+
+        x, det = hk.GRU(self.config.deterministic_size, w_i_init=initializer(self.initialization), w_h_init=hk.initializers.Orthogonal())(x, det)
+
+        x = hk.Linear(self.config.hidden, name='h2', w_init=initializer(self.initialization))(x)
+        x = jnn.elu(x)
+
+        # Creat Dist
+        x = hk.Linear(self.config.stochastic_size * 2, name='h3', w_init=initializer(self.initialization))(x)
         mean, stddev = jnp.split(x, 2, -1)
         stddev = jnn.softplus(stddev) + 0.1
-        prior = tfd.MultivariateNormalDiag(mean, stddev)
-        sample = prior.sample(seed=hk.next_rng_key())
-        return prior, (sample, det)
+        z_head_dist = tfd.MultivariateNormalDiag(mean, stddev)
+
+        # Sample from Dist
+        sample = z_head_dist.sample(seed=hk.next_rng_key())
+
+        # stack state
+        state = (sample, det)
+
+        return z_head_dist, state
 
 
-class Posterior(hk.Module):
+class Z_Layer(hk.Module):
     def __init__(self, config: RssmConfig, inititialization: str):
         super().__init__()
-        self.c = config
+
+        self.config = config
         self.initialization = inititialization
 
     def __call__(self, prev_state: State, observation: Observation) -> Tuple[tfd.MultivariateNormalDiag, State]:
+
+        # Unstack state
         _, det = prev_state
         cat = jnp.concatenate([det, observation], -1)
-        x = jnn.elu(hk.Linear(self.c.hidden, name='h1',
-                    w_init=initializer(self.initialization))(cat))
-        x = hk.Linear(self.c.stochastic_size * 2, name='h2',
-                      w_init=initializer(self.initialization))(x)
+
+        x = hk.Linear(self.config.hidden, name='h1', w_init=initializer(self.initialization))(cat)
+        x = jnn.elu(x)
+
+        # Creat Dist
+        x = hk.Linear(self.config.stochastic_size * 2, name='h2', w_init=initializer(self.initialization))(x)
         mean, stddev = jnp.split(x, 2, -1)
         stddev = jnn.softplus(stddev) + 0.1
-        posterior = tfd.MultivariateNormalDiag(mean, stddev)
-        sample = posterior.sample(seed=hk.next_rng_key())
-        return posterior, (sample, det)
+        z_dist = tfd.MultivariateNormalDiag(mean, stddev)
+
+        # Sample from Dist
+        z_sample = z_dist.sample(seed=hk.next_rng_key())
+
+        # stack state
+        state = (z_sample, det)
+
+        return z_dist, state
 
 
-def init_state(
-    batch_size: int,
-    stochastic_size: int,
-    deterministic_size: int,
-    dtype: Optional[jnp.dtype] = jnp.float32
-) -> State:
-    return (jnp.zeros((batch_size, stochastic_size), dtype),
-            jnp.zeros((batch_size, deterministic_size), dtype))
+def init_state(batch_size: int, stochastic_size: int, deterministic_size: int, dtype: Optional[jnp.dtype] = jnp.float32) -> State:
+    return (jnp.zeros((batch_size, stochastic_size), dtype), jnp.zeros((batch_size, deterministic_size), dtype))
 
 
 class RSSM(hk.Module):
     def __init__(self, config: DreamerConfiguration):
         super().__init__()
-        self.c = config
-        self.prior = Prior(config.rssm, config.initialization)
-        self.posterior = Posterior(config.rssm, config.initialization)
 
-    def __call__(
-        self,
-        prev_state: State,
-        prev_action: Action,
-        observation: Observation
-    ) -> Tuple[Tuple[tfd.MultivariateNormalDiag, tfd.MultivariateNormalDiag], State]:
-        prior, state = self.prior(prev_state, prev_action)
-        posterior, state = self.posterior(state, observation)
-        return (prior, posterior), state
+        self.config = config
+        self.z_head = Z_Head_Layer(config.rssm, config.initialization)
+        self.z_ = Z_Layer(config.rssm, config.initialization)
 
-    def generate_sequence(
-        self,
-        initial_features: jnp.ndarray,
-        actor: hk.Transformed,
-        actor_params: hk.Params,
-        actions=None
-    ) -> jnp.ndarray:
-        def vec(state):
-            return jnp.concatenate(state, -1)
+    def __call__(self, prev_state: State, prev_action: Action, observation: Observation) \
+            -> Tuple[Tuple[tfd.MultivariateNormalDiag, tfd.MultivariateNormalDiag], State]:
 
-        horizon = self.c.imag_horizon if actions is None else actions.shape[1]
-        sequence = jnp.zeros(
-            (initial_features.shape[0], horizon,
-             self.c.rssm.stochastic_size + self.c.rssm.deterministic_size)
-        )
-        state = jnp.split(initial_features, (self.c.rssm.stochastic_size,), -1)
+        z_head_dist, state = self.z_head(prev_state, prev_action)
+        z_dist, state = self.z_(state, observation)
+
+        return (z_head_dist, z_dist), state
+
+    def generate_sequence(self, initial_features: jnp.ndarray, actor: hk.Transformed, actor_params: hk.Params, actions=None) -> jnp.ndarray:
+
+        horizon = self.config.imag_horizon if actions is None else actions.shape[1]
+        sequence = jnp.zeros((initial_features.shape[0], horizon, self.config.rssm.stochastic_size + self.config.rssm.deterministic_size))
+        state = jnp.split(initial_features, (self.config.rssm.stochastic_size,), -1)
         keys = hk.next_rng_keys(horizon)
+
         for t, key in enumerate(keys):
-            action = actor.apply(
-                actor_params,
-                key,
-                jax.lax.stop_gradient(vec(state))
-            ).sample(seed=key) if actions is None else actions[:, t]
-            _, state = self.prior(state, action)
-            sequence = sequence.at[:, t].set(vec(state))
+
+            state_stack = jnp.concatenate(state, -1)
+            state_stack_stop = jax.lax.stop_gradient(state_stack)
+
+            action_dist = actor.apply(actor_params, key, state_stack_stop)
+
+            if actions is None:
+                action = action_dist.sample(seed=key)
+            else:
+                action = actions[:, t]
+
+            _, state = self.z_head(state, action)
+
+            sequence = sequence.at[:, t].set(state_stack)
+
         return sequence
 
-    def observe_sequence(
-        self,
-        observations: Observation,
-        actions: Action
-    ) -> Tuple[Tuple[tfd.MultivariateNormalDiag, tfd.MultivariateNormalDiag], jnp.ndarray]:
-        priors, posteriors = [], []
-        features = jnp.zeros(
-            observations.shape[:2] + (self.c.rssm.stochastic_size + self.c.rssm.deterministic_size,))
-        state = init_state(
-            observations.shape[0], self.c.rssm.stochastic_size, self.c.rssm.deterministic_size)
+    def observe_sequence(self, observations: Observation, actions: Action) \
+            -> Tuple[Tuple[tfd.MultivariateNormalDiag, tfd.MultivariateNormalDiag], jnp.ndarray]:
+
+        z_heads, z_s = [], []
+        features = jnp.zeros(observations.shape[:2] + (self.config.rssm.stochastic_size + self.config.rssm.deterministic_size,))
+        state = init_state(observations.shape[0], self.config.rssm.stochastic_size, self.config.rssm.deterministic_size)
+
+        # Unroll over sequenz
         for t in range(observations.shape[1]):
-            (prior, posterior), state = self.__call__(
-                state, actions[:, t], observations[:, t])
-            priors.append((prior.mean(), prior.stddev()))
-            posteriors.append((posterior.mean(), posterior.stddev()))
+            (z_head_dist, z_dist), state = self.__call__(state, actions[:, t], observations[:, t])
+
+            # Add mean and stddev to buffer list.
+            z_heads.append((z_head_dist.mean(), z_head_dist.stddev()))
+            z_s.append((z_dist.mean(), z_dist.stddev()))
+
+            # Set features at time stap t
             features = features.at[:, t].set(jnp.concatenate(state, -1))
 
         def joint_mvn(dists):
-            mus, stddevs = jnp.asarray(
-                list(zip(*dists))).transpose((0, 2, 1, 3))
+            mus, stddevs = jnp.asarray(list(zip(*dists))).transpose((0, 2, 1, 3))
             return tfd.MultivariateNormalDiag(mus, stddevs)
 
-        prior = joint_mvn(priors)
-        posterior = joint_mvn(posteriors)
-        return (prior, posterior), features
+        # Stack unroald mean and stddev
+        z_head = joint_mvn(z_heads)
+        z_ = joint_mvn(z_s)
+
+        return (z_head, z_), features
